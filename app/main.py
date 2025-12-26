af34311741896d1f75bc2784b259b46310b4ec32
Roman Kostiv <rkostiv@gmail.com>
2025-12-26 21:49:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
             serializer = serializer_cls()
             result = serializer.serialize(book)
 
-        return result
+    return result
 
 
 if __name__ == "__main__":
